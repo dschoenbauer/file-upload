66c9b2a317e5ed3f9fd3941058b337dd24ee68b0
David Schoenbauer <dschoenbauer@gmail.com>
2017-02-21 14:44:10 -0600

    Initial Commit

---
+++ src/FileUpload/FileUpload.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DSchoenbauer\FileUpload;
+
+/**
+ * Description of FileUpload
+ *
+ * @author David Schoenbauer <dschoenbauer@gmail.com>
+ */
+class FileUpload {
+    //put your code here
+}
